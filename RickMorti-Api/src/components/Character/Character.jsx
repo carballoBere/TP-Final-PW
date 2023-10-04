d58445b02a748d04e2f909cc5885b0f578b0a699
adriana muruchi <carballoadriana2856@gmail.com>
2023-10-04 16:58:27 -0300

    agregue titulo

--- RickMorti-Api/src/components/Character/Character.jsx
+++ RickMorti-Api/src/components/Character/Character.jsx
@@ -10,20 +10,30 @@ export function Character() {
   }, []);
 
   return (
-    <ul className="contenedor">
-      {allCharacters.map((item, index) => (
-        <li key={index}>
-          <div className="encapsulador">
-            <img src={item.image} alt="" />
-            <div>
-              <h3>{item.name}</h3>
-              <p>{item.status}</p>
-              <p>{item.origin.name}</p>
-              <p>{item.species}</p>
+    <div>
+      <div className="botones">
+        <button>Docs</button>
+        <button>About</button>
+        <button>Suport Us</button>
+      </div>
+      <div>
+        <h1>The Ricky And Morty API</h1>
+      </div>
+      <ul className="contenedor">
+        {allCharacters.map((item, index) => (
+          <li key={index}>
+            <div className="encapsulador">
+              <img src={item.image} alt="" />
+              <div className="texto">
+                <h3>{item.name}</h3>
+                <p>{item.status}</p>
+                <p>{item.origin.name}</p>
+                <p>{item.species}</p>
+              </div>
             </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
